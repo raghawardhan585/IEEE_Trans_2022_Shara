f5a1a6d018ea08ea8776eaaaf1b0d6ad0f1fe9f9
raghawardhan585 <raghawardhan585@gmail.com>
2022-04-11 00:53:18 -0700

    Intiate sytems 20 series

--- microtensor_run.sh
+++ microtensor_run.sh
@@ -1,52 +1,100 @@
 #!/bin/bash 
-rm -rf _current_run_saved_files 
-mkdir _current_run_saved_files 
-rm -rf Run_info 
-mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [n_observables] [n_layers] [n_nodes] [write_to_file] 
-python3 deepDMD.py '/cpu:0' 27 0 0 3 0 > System_27/MyRunInfo/Run_0.txt & 
+python3 deepDMD.py '/cpu:0' 25 0 0 3 0 > System_25/MyRunInfo/Run_0.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 1 0 4 0 > System_27/MyRunInfo/Run_1.txt & 
+python3 deepDMD.py '/cpu:0' 25 1 0 4 0 > System_25/MyRunInfo/Run_1.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 2 1 3 2 > System_27/MyRunInfo/Run_2.txt & 
+python3 deepDMD.py '/cpu:0' 25 2 1 3 2 > System_25/MyRunInfo/Run_2.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 3 1 4 2 > System_27/MyRunInfo/Run_3.txt & 
+python3 deepDMD.py '/cpu:0' 25 3 1 4 2 > System_25/MyRunInfo/Run_3.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 4 2 3 3 > System_27/MyRunInfo/Run_4.txt & 
+python3 deepDMD.py '/cpu:0' 25 4 2 3 3 > System_25/MyRunInfo/Run_4.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 5 2 4 3 > System_27/MyRunInfo/Run_5.txt & 
+python3 deepDMD.py '/cpu:0' 25 5 2 4 3 > System_25/MyRunInfo/Run_5.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 6 3 3 5 > System_27/MyRunInfo/Run_6.txt & 
+python3 deepDMD.py '/cpu:0' 25 6 3 3 5 > System_25/MyRunInfo/Run_6.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 7 3 4 5 > System_27/MyRunInfo/Run_7.txt & 
+python3 deepDMD.py '/cpu:0' 25 7 3 4 5 > System_25/MyRunInfo/Run_7.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 8 4 3 6 > System_27/MyRunInfo/Run_8.txt & 
+python3 deepDMD.py '/cpu:0' 25 8 4 3 6 > System_25/MyRunInfo/Run_8.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 9 4 4 6 > System_27/MyRunInfo/Run_9.txt & 
+python3 deepDMD.py '/cpu:0' 25 9 4 4 6 > System_25/MyRunInfo/Run_9.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 10 5 3 8 > System_27/MyRunInfo/Run_10.txt & 
+python3 deepDMD.py '/cpu:0' 25 10 5 3 8 > System_25/MyRunInfo/Run_10.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 11 5 4 8 > System_27/MyRunInfo/Run_11.txt & 
+python3 deepDMD.py '/cpu:0' 25 11 5 4 8 > System_25/MyRunInfo/Run_11.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 12 6 3 9 > System_27/MyRunInfo/Run_12.txt & 
+python3 deepDMD.py '/cpu:0' 26 0 0 3 0 > System_26/MyRunInfo/Run_0.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 13 6 4 9 > System_27/MyRunInfo/Run_13.txt & 
+python3 deepDMD.py '/cpu:0' 26 1 0 4 0 > System_26/MyRunInfo/Run_1.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 14 7 3 11 > System_27/MyRunInfo/Run_14.txt & 
+python3 deepDMD.py '/cpu:0' 26 2 1 3 2 > System_26/MyRunInfo/Run_2.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 15 7 4 11 > System_27/MyRunInfo/Run_15.txt & 
+python3 deepDMD.py '/cpu:0' 26 3 1 4 2 > System_26/MyRunInfo/Run_3.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 16 8 3 12 > System_27/MyRunInfo/Run_16.txt & 
+python3 deepDMD.py '/cpu:0' 26 4 2 3 3 > System_26/MyRunInfo/Run_4.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 17 8 4 12 > System_27/MyRunInfo/Run_17.txt & 
+python3 deepDMD.py '/cpu:0' 26 5 2 4 3 > System_26/MyRunInfo/Run_5.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 18 9 3 14 > System_27/MyRunInfo/Run_18.txt & 
+python3 deepDMD.py '/cpu:0' 26 6 3 3 5 > System_26/MyRunInfo/Run_6.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 19 9 4 14 > System_27/MyRunInfo/Run_19.txt & 
+python3 deepDMD.py '/cpu:0' 26 7 3 4 5 > System_26/MyRunInfo/Run_7.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 20 10 3 15 > System_27/MyRunInfo/Run_20.txt & 
+python3 deepDMD.py '/cpu:0' 26 8 4 3 6 > System_26/MyRunInfo/Run_8.txt & 
 wait
-python3 deepDMD.py '/cpu:0' 27 21 10 4 15 > System_27/MyRunInfo/Run_21.txt & 
+python3 deepDMD.py '/cpu:0' 26 9 4 4 6 > System_26/MyRunInfo/Run_9.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 26 10 5 3 8 > System_26/MyRunInfo/Run_10.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 26 11 5 4 8 > System_26/MyRunInfo/Run_11.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 0 0 3 0 > System_28/MyRunInfo/Run_0.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 1 0 4 0 > System_28/MyRunInfo/Run_1.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 2 1 3 2 > System_28/MyRunInfo/Run_2.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 3 1 4 2 > System_28/MyRunInfo/Run_3.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 4 2 3 3 > System_28/MyRunInfo/Run_4.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 5 2 4 3 > System_28/MyRunInfo/Run_5.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 6 3 3 5 > System_28/MyRunInfo/Run_6.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 7 3 4 5 > System_28/MyRunInfo/Run_7.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 8 4 3 6 > System_28/MyRunInfo/Run_8.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 9 4 4 6 > System_28/MyRunInfo/Run_9.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 10 5 3 8 > System_28/MyRunInfo/Run_10.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 28 11 5 4 8 > System_28/MyRunInfo/Run_11.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 0 0 3 0 > System_29/MyRunInfo/Run_0.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 1 0 4 0 > System_29/MyRunInfo/Run_1.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 2 1 3 2 > System_29/MyRunInfo/Run_2.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 3 1 4 2 > System_29/MyRunInfo/Run_3.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 4 2 3 3 > System_29/MyRunInfo/Run_4.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 5 2 4 3 > System_29/MyRunInfo/Run_5.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 6 3 3 5 > System_29/MyRunInfo/Run_6.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 7 3 4 5 > System_29/MyRunInfo/Run_7.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 8 4 3 6 > System_29/MyRunInfo/Run_8.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 9 4 4 6 > System_29/MyRunInfo/Run_9.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 10 5 3 8 > System_29/MyRunInfo/Run_10.txt & 
+wait
+python3 deepDMD.py '/cpu:0' 29 11 5 4 8 > System_29/MyRunInfo/Run_11.txt & 
 wait
 echo "All sessions are complete" 
 echo "=======================================================" 
